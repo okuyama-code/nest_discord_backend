abb5bd14414c257b28b173f4d16ad7e528492eca
okuyama-code <ooyy0121@gmail.com>
2024-05-18 11:49:56 +0900

    createServerメソッドの作成まで

--- src/server/server.service.ts
+++ src/server/server.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateServerDto } from './dto';
+import { v4 as uuidv4 } from 'uuid';
+import { MemberRole } from 'src/member/member.types';
 
 @Injectable()
-export class ServerService {}
+export class ServerService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createServer(input: CreateServerDto, imageUrl: string) {
+    const profile = await this.prisma.profile.findUnique({
+      where: {
+        id: input.profileId,
+      },
+    });
+    if (!profile) throw new BadRequestException('Profile not found')
+
+    return this.prisma.server.create({
+      data: {
+        ...input,
+        imageUrl,
+        inviteCode: uuidv4(),
+
+        channels: {
+          create: [
+            {
+              name: 'general',
+              profileId: profile.id,
+            },
+          ],
+        },
+        members: {
+          create: [
+            {
+              profileId: profile.id,
+              role: MemberRole.ADMIN,
+            },
+          ],
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+  }
+}
